fc30d711ad5628c249c4f79117b7917ec465fedd
root <root@DESKTOP-04SM3DR>
2024-03-06 17:09:02 -0300

    3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,11 @@ const RootLayout = ({ children }: { children: any }) => {
       console.log('entrou aq')
     }
   }, []);
-
+  ReactGA.initialize('G-JRDW7FJ9XZ'); 
+  ReactGA.send({
+    hitType:"pageview",
+    page: window.location.pathname
+  })
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
