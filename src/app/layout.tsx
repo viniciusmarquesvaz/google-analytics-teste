6a8be7154c1d74fba96373897707d797ea56648b
root <root@DESKTOP-04SM3DR>
2024-03-06 17:15:40 -0300

    terceira tentaiva

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,38 @@
 'use client'
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import ReactGA from 'react-ga';
-
+import Script from 'next/script';
 
 import { Inter } from 'next/font/google';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
-
 const RootLayout = ({ children }: { children: any }) => {
   useEffect(() => {
     if (typeof window !== 'undefined') {
-      ReactGA.initialize('G-JRDW7FJ9XZ'); 
+      // Código do Google Analytics
+      ReactGA.initialize('G-JRDW7FJ9XZ');
       ReactGA.pageview(window.location.pathname + window.location.search);
-      console.log('entrou aq')
+      console.log('entrou aq');
     }
   }, []);
-  ReactGA.initialize('G-JRDW7FJ9XZ'); 
-  ReactGA.send({
-    hitType:"pageview",
-    page: window.location.pathname
-  })
+
   return (
     <html lang="en">
+      <head>
+        {/* Adiciona o script do Google Analytics diretamente no head */}
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-JRDW7FJ9XZ" />
+        <Script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-JRDW7FJ9XZ');
+          `}
+        </Script>
+        {/* ... outros meta tags e links de estilo ... */}
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
